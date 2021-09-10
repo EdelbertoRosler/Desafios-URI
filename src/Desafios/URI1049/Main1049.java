ab185b0691abf05711fb9f98b8e818c554e1a968
EdelbertoRosler <edelbertorosler@gmail.com>
2021-09-10 00:28:56 -0300

    correções

--- src/Desafios/URI1049/Main1049.java
+++ src/Desafios/URI1049/Main1049.java
@@ -10,15 +10,37 @@ public class Main1049 {
         Scanner scan = new Scanner(System.in);
         String one = scan.next(); String two = scan.next(); String three = scan.next();
 
-        if (one.equals("vertebrado") && two.equals("ave") && three.equals("carnivoro")){System.out.println("aguia");}
-        if (one.equals("vertebrado") && two.equals("ave") && three.equals("onivoro")){System.out.println("pomba");}
-        if (one.equals("vertebrado") && two.equals("mamifero") && three.equals("onivoro")){System.out.println("homem");}
-        if (one.equals("vertebrado") && two.equals("mamifero") && three.equals("herbivoro")){System.out.println("vaca");}
+        if (one.equals("vertebrado") && two.equals("ave") && three.equals("carnivoro")){ System.out.println("aguia"); }
+        else {
+            if (one.equals("vertebrado") && two.equals("ave") && three.equals("onivoro")) {
+                System.out.println("pomba");
+            } else {
+                if (one.equals("vertebrado") && two.equals("mamifero") && three.equals("onivoro")) {
+                    System.out.println("homem");
+                }
+                else {
+                    if (one.equals("vertebrado") && two.equals("mamifero") && three.equals("herbivoro")){
+                        System.out.println("vaca");}
+                }
+            }
+        }
 
         if (one.equals("invertebrado") && two.equals("inseto") && three.equals("hematofago")){System.out.println("pulga");}
-        if (one.equals("invertebrado") && two.equals("inseto") && three.equals("herbivoro")){System.out.println("lagarta");}
-        if (one.equals("invertebrado") && two.equals("anelideo") && three.equals("hematofago")){System.out.println("sanguessuga");}
-        if (one.equals("invertebrado") && two.equals("anelideo") && three.equals("onivoro")){System.out.println("minhoca");}
+        else {
+            if (one.equals("invertebrado") && two.equals("inseto") && three.equals("herbivoro")) {
+                System.out.println("lagarta");
+            } else {
+                if (one.equals("invertebrado") && two.equals("anelideo") && three.equals("hematofago")){
+                    System.out.println("sanguessuga");
+                }
+                else {
+                    if (one.equals("invertebrado") && two.equals("anelideo") && three.equals("onivoro")){
+                        System.out.println("minhoca");
+                    }
+                }
+
+            }
+        }
 
     }
 }
--- src/Desafios/desafioDIO/Uniformes.java
+++ src/Desafios/desafioDIO/Uniformes.java
@@ -1,74 +1,100 @@
 package Desafios.desafioDIO;
 
-
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.io.PrintWriter;
+        import java.util.Arrays;
+        import java.util.Comparator;
 
 public class Uniformes {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int linhas = Integer.parseInt(st.nextToken());
-        List<Uniforme> uniformes = new ArrayList<>();
-
-        for (int i = 0; i <= linhas; i++) {
-            var next = br.readLine();
-            if(next.equals("0")) {
-                break;
+        String owner, l, color, size;
+        String[] details;
+        Tshirt[] tshirts;
+        Tshirt tshirt;
+        int N;
+        boolean firstCase = true;
+        while (!(l = read()).equals("0")) {
+            if (firstCase) {
+                firstCase = false;
+            } else {
+                out.println();
             }
-            Uniforme uniforme = new Uniforme();
-            uniforme.setNomeEstudante(next);
-            var camiseta = br.readLine().split(" ");
-            uniforme.setCorUniforme(camiseta[0]);
-            uniforme.setTamUniforme(camiseta[1]);
-            uniformes.add(uniforme);
+            N = toInt(l);
+            tshirts = new Tshirt[N];
+            for (int i = 0; i < N; i++) {
+                owner = read();
+                details = read().split("\\s");
+                color = details[0];
+                size = details[1];
+                tshirt = new Tshirt(size, color, owner);
+                tshirts[i] = tshirt;
+            }
+            Arrays.sort(tshirts, new Comparator<Tshirt>() {
+                @Override
+                public int compare(Tshirt t1, Tshirt t2) {
+                    String color1 = t1.getColor();
+                    String color2 = t2.getColor();
+                    int diff = color1.compareTo(color2);
+                    if (diff != 0) {
+                        return diff;
+                    } else {
+                        String s1 = t1.getSize();
+                        String s2 = t2.getSize();
+                        if (s1.equals(s2)) {
+                            return t1.getOwner().compareTo(t2.getOwner());
+                        }
+                        if (s1.equals("P") && s2.equals("M")
+                                || s1.equals("P") && s2.equals("G")
+                                || s1.equals("M") && s2.equals("G")) {
+                            return -1;
+                        }
+                        return 1;
+                    }
+                }
+            });
+            for (Tshirt t : tshirts) {
+                out.println(t);
+            }
         }
-        Comparator<Uniforme> comparator = Comparator
-                .comparing(Uniforme::getCorUniforme).reversed()
-                .thenComparing(Uniforme::getTamUniforme).reversed()
-                .thenComparing(Uniforme::getNomeEstudante);
-
-        uniformes.stream().sorted(comparator).forEach(System.out::println);
+        out.close();
     }
 
-    public static class Uniforme {
-        private String nomeEstudante;
-        private String corUniforme;
-        private String tamUniforme;
+    private static String read() throws IOException { return in.readLine(); }
+    private static int toInt(String s) { return Integer.parseInt(s); }
+}
 
-        @Override
-        public String toString() {
-            return corUniforme + " " + tamUniforme + " "+ nomeEstudante;
-        }
+class Tshirt {
 
-        public String getNomeEstudante() {
-            return nomeEstudante;
-        }
+    private String size;
+    private String color;
+    private String owner;
 
-        public void setNomeEstudante(String nomeEstudante) {
-            this.nomeEstudante = nomeEstudante;
-        }
-
-        public String getCorUniforme() {
-            return corUniforme;
-        }
-
-        public void setCorUniforme(String corUniforme) {
-            this.corUniforme = corUniforme;
-        }
-
-        public String getTamUniforme() {
-            return tamUniforme;
-        }
-
-        public void setTamUniforme(String tamUniforme) {
-            this.tamUniforme = tamUniforme;
-        }
+    public Tshirt(String size, String color, String owner) {
+        this.size = size;
+        this.color = color;
+        this.owner = owner;
     }
+
+    public String getSize() { return size; }
+
+    public void setSize(String size) { this.size = size; }
+
+    public String getColor() { return color; }
+
+    public void setColor(String color) { this.color = color; }
+
+    public String getOwner() { return owner; }
+
+    public void setOwner(String owner) { this.owner = owner; }
+
+    @Override
+    public String toString() {
+        return this.getColor() + " " + this.getSize() + " " + this.getOwner();
     }
 }
